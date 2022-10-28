1b42686dc977fba48a7562ee127c38e8155a640b
Thomas Michael Fjærvik <Thomas.Fjaervik@nhh.no>
2022-10-28 13:54:23 +0200

    Added function write_tex()

--- R/textools.R
+++ R/textools.R
@@ -44,3 +44,45 @@ save_tikz <- function(fig = NULL, path = NULL, sanitize = TRUE) {
   invisible(path)
 
 }
+
+
+#' Write text output to .tex file
+#'
+#' @param x Data to be written to .tex file
+#' @param path Character, file name to store plot (.tex file)
+#' @param folder Character. Folder location relative to project file location
+#'
+#' @return Return the data x without printing it. Hence, x may be assigned something.
+#' @export
+
+
+write_tex <- function(x, path, folder) {
+
+  if (!is.character(path))
+    stop("Please specify a correct path.")
+
+  if (!endsWith(path, ".tex")) {
+    warning("File extension is not tex, extension is changed",
+            call. = F)
+  # if (!is_tex(path)) {
+  #   warning("File extension is not tex, extension is changed",
+  #           call. = F)
+
+
+
+    path <- fs::path_ext_set(fs::path(folder,fs::path_ext_remove(path)), ext = "tex")
+  }
+
+
+  #path <- normalizePath(path, mustWork = TRUE)
+
+  file_conn <- file(path)
+  cat(x, file = file_conn, sep = "\n")
+  close(file_conn)
+
+  utils::writeClipboard(x)
+
+  return(invisible(x))
+}
+
+write_tex(x, "yo", "man")
